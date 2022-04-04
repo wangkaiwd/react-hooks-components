4ea1aeedf8e286f703f4d9a22de943fb43de7e09
wangkaiwd <1162486176@qq.com>
2022-04-04 23:22:03 +0800

    feat: support scss

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './assets/styles/index.less';
+import './assets/styles/index.scss';
 import App from './App';
 
 ReactDOM.render(

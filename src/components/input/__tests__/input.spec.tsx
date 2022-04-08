45083c3ce0e2b346b68d648090d0061ab4480c5b
wangkaiwd <1162486176@qq.com>
2022-04-08 13:38:56 +0800

    feat: create snapshot for input component

--- src/components/input/__tests__/input.spec.tsx
+++ src/components/input/__tests__/input.spec.tsx
@@ -1,24 +1,29 @@
-import { fireEvent, render, screen } from "@testing-library/react";
-import Input from "../input";
+import { fireEvent, render, screen } from '@testing-library/react';
+import renderer from 'react-test-renderer';
+import Input from '../input';
 
-describe("Input", () => {
-  it("should render input", () => {
-    const { container } = render(<Input />);
-    expect(container.querySelector("input")).toBeInTheDocument();
+describe('Input', () => {
+  it('should render correctly', () => {
+    const input = renderer.create(<Input placeholder="Please input something..."/>).toJSON();
+    expect(input).toMatchSnapshot();
   });
-  it("should clear text within input", () => {
+  it('should clear text within input', () => {
     const onChange = jest.fn();
-    render(<Input onChange={onChange} />);
-    const input = screen.getByTestId<HTMLInputElement>("input");
-    input.value = "20";
-    fireEvent.click(screen.getByTestId("clear"));
+    render(<Input onChange={onChange}/>);
+    const input = screen.getByTestId<HTMLInputElement>('input');
+    input.value = '20';
+    fireEvent.click(screen.getByTestId('clear'));
     expect(onChange).toBeCalledTimes(1);
-    expect(screen.getByTestId("input")).toHaveValue("");
+    expect(screen.getByTestId('input')).toHaveValue('');
   });
-  it("should set value and change value", () => {
-    // render(<Input value={10}/>);
+  it('should trigger change with parameter value', () => {
+    const onChange = jest.fn();
+    render(<Input onChange={onChange}/>);
+    const input = screen.getByTestId('input');
+    fireEvent.input(input, { target: { value: '10' } });
+    expect(onChange).toHaveBeenCalledTimes(1);
+    expect(onChange).toHaveBeenCalledWith('10');
   });
-  it("should support all attributes of native input", () => {
-
+  it('should support all attributes of native input', () => {
   });
 });
--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -1,9 +1,8 @@
-import React, { FormEvent, InputHTMLAttributes, useMemo, useRef } from "react";
-import cls from "classnames";
-import "./input.scss";
+import React, { FormEvent, InputHTMLAttributes, useMemo, useRef } from 'react';
+import cls from 'classnames';
+import './input.scss';
 
-// @ts-ignore
-interface Props extends InputHTMLAttributes<HTMLInputElement> {
+interface Props extends Omit<InputHTMLAttributes<HTMLInputElement>, 'onChange'> {
   allowClear?: boolean;
   onChange?: (value: string) => void;
 }
@@ -12,18 +11,18 @@ const Input: React.FC<Props> = (props) => {
   const { className, allowClear, onChange, ...nativeProps } = props;
   const inputRef = useRef<HTMLInputElement>(null!);
   const classes = useMemo(() => {
-    return cls("ant-input", className, { clear: allowClear });
+    return cls('ant-input', className, { clear: allowClear });
   }, [className, allowClear]);
   const onClear = () => {
-    inputRef.current.value = "";
+    inputRef.current.value = '';
     onChange?.(inputRef.current.value);
   };
-  const onInput = (e: FormEvent) => {
+  const onInput = (e: FormEvent<HTMLInputElement>) => {
     onChange?.((e.target as HTMLInputElement).value);
   };
   return (
     <div className={classes}>
-      <input data-testid="input" ref={inputRef} {...nativeProps} onInput={onInput} />
+      <input data-testid="input" ref={inputRef} {...nativeProps} onInput={onInput}/>
       <div data-testid="clear" className="ant-input-clear" onClick={onClear}>
         x
       </div>

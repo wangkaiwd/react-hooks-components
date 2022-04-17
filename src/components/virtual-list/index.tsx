5554d59f822d3615ff8bdc4441089098952e2826
wangkaiwd <1162486176@qq.com>
2022-04-18 00:44:52 +0800

    chore: handle className and style props of component

--- src/components/virtual-list/index.tsx
+++ src/components/virtual-list/index.tsx
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { CSSProperties } from "react";
+import "./index.scss";
+import cls from "classnames";
 
-const VirtualList = () => {
+const prefixCls = "ant-virtual-list";
+
+interface VirtualListProps {
+  className?: string;
+  style?: CSSProperties;
+}
+
+const VirtualList = (props: VirtualListProps) => {
+  const { className, ...rest } = props;
+  const classes = cls(prefixCls, className);
+
   return (
-    <div>
+    <div {...rest} className={classes}>
       virtual list
     </div>
   );

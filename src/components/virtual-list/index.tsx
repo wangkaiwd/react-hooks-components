390102a41b33e89cff76fa5b220b2ef098ace885
wangkaiwd <1162486176@qq.com>
2022-04-21 21:07:03 +0800

    chore(develop): generate first rows

--- src/components/virtual-list/index.tsx
+++ src/components/virtual-list/index.tsx
@@ -17,16 +17,32 @@ const list = new Array(10000).fill(true).map((item, i) => ({
   lastName: Math.random().toString(20).substring(8),
   age: Math.ceil(Math.random() * 80)
 }));
-
+const listHeight = rowHeight * list.length;
 const VirtualList = (props: VirtualListProps) => {
   const { className, ...rest } = props;
   const classes = cls(prefixCls, className);
   const [state, setState] = useSetState({
     index: 0,
   });
+  const getRows = () => {
+    const rows = [];
+    let i = state.index;
+    const end = state.index + 20;
+    for (; i < end; i++) {
+      rows.push(
+        <div className={`${prefixCls}-item`}>
+          {i}
+        </div>
+      );
+    }
+    setState({ index: i });
+    return rows;
+  };
   return (
-    <div {...rest} className={classes}>
-      virtual list
+    <div {...rest} className={classes} style={{ height: `${rowHeight * 10}px` }}>
+      <div className={`${prefixCls}-list`} style={{ height: `${listHeight}px` }}>
+        {getRows()}
+      </div>
     </div>
   );
 };

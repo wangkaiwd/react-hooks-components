2ee997e80dc4e76cf98d80d0b674b7da4d3e3780
wangkaiwd <1162486176@qq.com>
2022-04-21 20:36:41 +0800

    chore(develop): add demo list for virtual list

--- src/components/virtual-list/index.tsx
+++ src/components/virtual-list/index.tsx
@@ -1,18 +1,29 @@
-import React, { CSSProperties } from "react";
-import "./index.scss";
-import cls from "classnames";
+import React, { CSSProperties } from 'react';
+import './index.scss';
+import cls from 'classnames';
+import useSetState from '../../hooks/useSetState';
 
-const prefixCls = "ant-virtual-list";
+const prefixCls = 'ant-virtual-list';
 
 interface VirtualListProps {
   className?: string;
   style?: CSSProperties;
 }
 
+const rowHeight = 20;
+const list = new Array(10000).fill(true).map((item, i) => ({
+  id: i,
+  firstName: Math.random().toString(20).substring(8),
+  lastName: Math.random().toString(20).substring(8),
+  age: Math.ceil(Math.random() * 80)
+}));
+
 const VirtualList = (props: VirtualListProps) => {
   const { className, ...rest } = props;
   const classes = cls(prefixCls, className);
-
+  const [state, setState] = useSetState({
+    index: 0,
+  });
   return (
     <div {...rest} className={classes}>
       virtual list

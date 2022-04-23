00100f21852099c8a29c20b8fb5e10ef81f672e4
wangkaiwd <1162486176@qq.com>
2022-04-23 23:32:37 +0800

    feat: implement basic version of virtual list

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import VirtualList from "./components/virtual-list";
+import "./styles/index.scss";
 
 function App () {
   return (
-    <div className="App">
+    <div className="App" style={{ padding: "20px" }}>
+      <h2>This is a list</h2>
       <VirtualList></VirtualList>
+      <h2>End</h2>
     </div>
   );
 }
--- src/components/virtual-list/index.tsx
+++ src/components/virtual-list/index.tsx
@@ -1,16 +1,16 @@
-import React, { CSSProperties } from 'react';
-import './index.scss';
-import cls from 'classnames';
-import useSetState from '../../hooks/useSetState';
+import React, { CSSProperties, UIEvent } from "react";
+import "./index.scss";
+import cls from "classnames";
+import useSetState from "../../hooks/useSetState";
 
-const prefixCls = 'ant-virtual-list';
+const prefixCls = "ant-virtual-list";
 
 interface VirtualListProps {
   className?: string;
   style?: CSSProperties;
 }
 
-const rowHeight = 20;
+const rowHeight = 30;
 const list = new Array(10000).fill(true).map((item, i) => ({
   id: i,
   firstName: Math.random().toString(20).substring(8),
@@ -22,25 +22,44 @@ const VirtualList = (props: VirtualListProps) => {
   const { className, ...rest } = props;
   const classes = cls(prefixCls, className);
   const [state, setState] = useSetState({
-    index: 0,
+    start: 0
   });
+  const onScroll = (e: UIEvent<HTMLDivElement>) => {
+    const scrollTop = (e.target as HTMLDivElement).scrollTop;
+    const distance = Math.floor(scrollTop / rowHeight);
+    setState({ start: distance });
+  };
   const getRows = () => {
     const rows = [];
-    let i = state.index;
-    const end = state.index + 20;
+    let i = state.start;
+    const end = state.start + 20;
+    const rowStyles: CSSProperties = {
+      position: "absolute",
+      height: `${rowHeight}px`,
+      border: "1px solid red",
+      lineHeight: `${rowHeight}px`,
+      width: "100%"
+    };
+    // position absolute element top always based on closest position relative parent initial left top corner
+    // if scroll position absolute element:
+    //  1. set position based on initial position relative parent
+    //  2. then scroll it
+    //  3. if position relative parent also scroll, the pivot always is left corner of first render and not change over scroll
     for (; i < end; i++) {
       rows.push(
-        <div className={`${prefixCls}-item`}>
-          {i}
+        <div className={`${prefixCls}-item`} key={i}
+             style={{ ...rowStyles, top: `${i * rowHeight}px` }}>
+          {i + 1}
         </div>
       );
     }
-    setState({ index: i });
     return rows;
   };
   return (
-    <div {...rest} className={classes} style={{ height: `${rowHeight * 10}px` }}>
-      <div className={`${prefixCls}-list`} style={{ height: `${listHeight}px` }}>
+    <div {...rest} className={classes}
+         style={{ height: `${rowHeight * 10}px`, overflowY: "scroll" }}
+         onScroll={onScroll}>
+      <div className={`${prefixCls}-list`} style={{ height: `${listHeight}px`, position: "relative" }}>
         {getRows()}
       </div>
     </div>

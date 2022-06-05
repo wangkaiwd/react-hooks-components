40109c9ef637551baf599c69aeefb5affdaf80c9
wangkaiwd <1162486176@qq.com>
2022-06-05 18:54:12 +0800

    docs: update deploy directory

--- dumi/config/config.js
+++ dumi/config/config.js
@@ -2,8 +2,8 @@ const path = require("path");
 const logo = "https://www.vectorlogo.zone/logos/reactjs/reactjs-icon.svg";
 const projectRoot = path.resolve(__dirname, "../../");
 const config = {
-  base: "/react-storybook",
-  publicPath: "/react-storybook/",
+  base: "/react-hooks-components",
+  publicPath: "/react-hooks-components/",
   exportStatic: {}, // Export all routes as HTML directory structure to avoid 404 when refreshing the page
   chainWebpack (memo) {
     memo.plugins.delete("copy");
